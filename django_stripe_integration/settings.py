a38f54401bc72058fd76d846b86c7c8f5b680b73
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-04-27 22:39:01 +0530

    worked on webhook. buyer can get mail about products after payment

--- django_stripe_integration/settings.py
+++ django_stripe_integration/settings.py
@@ -96,7 +96,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rudreshsoftsuave82@gmail.com'
+EMAIL_HOST_PASSWORD = 'thzxkhdcrmgpwcks'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
